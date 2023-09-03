4bfe33bddc98665f4114e261af3e8a3583a8a90c
alexmf91 <alexmunoz91@gmail.com>
2023-09-03 23:58:32 +0200

    feat: Add wagmi config

---
+++ config/wagmi.ts
@@ -0,0 +1,16 @@
+import { getDefaultConfig } from 'connectkit';
+import { createConfig } from 'wagmi';
+import { polygonMumbai } from 'wagmi/chains';
+
+const config = createConfig(
+  getDefaultConfig({
+    alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_ID,
+    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+    appName: 'Polygon Launchpad',
+    appDescription:
+      'Discover and participate in the most promising token presales on the Polygon network.',
+    chains: [polygonMumbai],
+  })
+);
+
+export default config;
