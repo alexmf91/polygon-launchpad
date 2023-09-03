9d28dc77080d47330c7bb6583fa4640a2c211d58
alexmf91 <alexmunoz91@gmail.com>
2023-09-04 00:39:24 +0200

    fix: remove unused hook

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,3 +1,2 @@
-export { default as useBlockTimestamp } from './useBlockTimestamp';
 export { default as useCountdown } from './useCountdown';
 export { default as useStageEndTime } from './useStageEndTime';
