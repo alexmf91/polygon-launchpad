cc8cb17b312fedef43a1534178676d7fe6cf0b80
alexmf91 <alexmunoz91@gmail.com>
2023-09-04 12:24:35 +0200

    feat: Add getEstimatedBlockCountdown function

--- utils/index.ts
+++ utils/index.ts
@@ -6,3 +6,13 @@ export function getContractErrorMessage(error: Error): string {
   }
   return error.message;
 }
+
+export default async function getEstimatedBlockCountdown(blockNumber: bigint): Promise<number> {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_POLYGON_SCAN_API_URL}/api?module=block&action=getblockcountdown&blockno=${blockNumber}&apikey=${process.env.NEXT_PUBLIC_POLYGON_SCAN_API_KEY}`
+  );
+
+  const { result } = await response.json();
+
+  return result.EstimateTimeInSec;
+}
